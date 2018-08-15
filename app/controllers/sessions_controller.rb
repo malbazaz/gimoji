2a5aaf23f8609ed39444b2ae284738dc766e502e
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-15 16:34:45 -0400

    Update login and register view. Define create and destroy for Sessionscontroller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create 
+  	if params[:username]
+    	@user = User.find_by(username: params[:username])
+	else
+		@user = User.find_by(email: params[:email])
+	end  
+	if @user
+    	return redirect_to 'signin' unless @user.authenticate(params[:password])
+    	session[:user_id] = @user.id 
+    	redirect_to "/users/#{@user.id}"
+    end
+    
+  end 
+
+  def destroy
+    session.delete :user_id 
+    redirect_to '/'
+  end 
+  
 end
