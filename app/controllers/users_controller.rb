5678fb3569fef42d01d828a2f85e687da9edb496
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-15 15:56:56 -0400

    Add new,create,index,show to UsersController. Delete additional unwanted devise file for View and rearranged login page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all 
+    end 
+
+    def new
+        @user = User.new  
+    end 
+    
+    def create 
+        @user= User.create(user_params)
+        session[:user_id]= @user.id
+    end 
+
+    def show 
+            if !!session[:user_id]
+                 @user = User.find_by_id(params[:id])
+            else 
+                redirect_to '/'
+            end
+    end 
+
+    def update 
+    end 
+
+    
+    private 
+
+    def user_params 
+        params.require(:user).permit(:fullname, :username, :password, :giftcoins, :admin)
+    end 
 end
+
