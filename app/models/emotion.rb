01d8a8f8cc86f29da85a3f6f8d90d39d0288c630
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-15 15:33:53 -0400

    Update relationships through models.

--- app/models/emotion.rb
+++ app/models/emotion.rb
@@ -1,2 +1,4 @@
 class Emotion < ApplicationRecord
+	has_many :giftmoji_emotions
+	has_many :gimojis, through: :giftmoji_emotions
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          has_many :giftmojis
+         has_many :giftmoji_emotions, through: :giftmojis 
+         has_many :emotions, through: :giftmoji_emotions
 
 end
