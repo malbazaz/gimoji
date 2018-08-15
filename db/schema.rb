4d7b71f025607c435cc0333a0629219dd047145e
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-15 13:05:12 -0400

    add gitcoins to users table and migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_163149) do
+ActiveRecord::Schema.define(version: 2018_08_15_170334) do
 
   create_table "giftmojis", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2018_08_15_163149) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.integer "giftcoins", default: 1000
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
