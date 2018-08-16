5b845b19e6acffdce4104ff08e9d65e4b489483a
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-16 12:04:05 -0400

    Update Giftmoji index view and create helper.

--- app/helpers/giftmojis_helper.rb
+++ app/helpers/giftmojis_helper.rb
@@ -14,7 +14,12 @@ module GiftmojisHelper
 		end  
 	end
 
+	def giftmoji_has_no_owner?
+		giftmoji.user_id == "" || giftmoji.user_id == nil || giftmoji.user_id == false
+	end 
+
 	def giftmoji_owner?
 		current_user == @giftmoji.user 
 	end 
+
 end
