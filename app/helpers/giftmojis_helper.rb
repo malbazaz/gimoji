0ca7c4da198d6e6d75848d8d7261a3442a19400b
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-16 11:50:56 -0400

    Update show view and create helpers.

--- app/helpers/giftmojis_helper.rb
+++ app/helpers/giftmojis_helper.rb
@@ -1,2 +1,20 @@
 module GiftmojisHelper
+
+	def current_user 
+	user = User.find_by_id(session[:user_id])
+	end 
+
+	def giftmoji_gifted? 
+		if @giftmoji.message == "" || @giftmoji.message == nil 
+			false 
+		elsif @giftmoji.occasion == "" || @giftmoji.occasion == nil 
+			false 
+		else 
+			true 
+		end  
+	end
+
+	def giftmoji_owner?
+		current_user == @giftmoji.user 
+	end 
 end
