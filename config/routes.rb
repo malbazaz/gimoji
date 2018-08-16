f0b53d2e9d94cfd6f7347aa734dfe129aef39e75
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-16 12:46:11 -0400

    Update route for buying.

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   get '/' =>'application#index'
   get '/users/:id' => 'users#show'
-  post '/giftmoji/:id/buy' => 'giftmojis#update'
+  post '/giftmoji/:id/buy' => 'users#update'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
